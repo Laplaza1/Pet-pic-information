15a3e5f19e401d101977c6c8a59202f6749e6ab5
Laplaza1 <142614580+Laplaza1@users.noreply.github.com>
2024-03-03 17:55:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-#Skyler Savage
-#1/21/2024
-#Pycharm Community
 
-
-#store variable to contain the user input to float   celsius = fahrenheit
-#store Variable to reresent the function to convert Celcius to Ferheinheitt  x = celsius * 9/5 +32
-#display the result of the function along with a string to inform the user that is the result of their input return x + Degrees Farhrenheit
-
-
-celsius = float(input("What temp would you like to convert to Fahrenheit? :"))
-
-
-f = (celsius * (9/5)+32)
-
-print(f"{f} Degrees Fahrenheit")
